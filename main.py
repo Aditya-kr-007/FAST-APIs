9eacde31ba6568f32db986ab382f6d51779feecc
Aditya-kr-007 <adikumar681@gmail.com>
2026-08-16 18:40:39 +0530

    add

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+
 app = FastAPI()
 
-posts: list[dict] = [
-    {
-        "id": 1,
-        "title": "Sony Software",
-        "body": "Amazing SW environment",
-        "content": "FastAPI framework is really easy to use and super",
-        "date_posted": "April 21, 2025",
-    },
-    {
-        "id": 2,
-        "title": "Demo Post",
-        "body": "Welcome to great for web development",
-        "content": "FastAPI is a great framework for web development",
-        "date_posted": "April 22, 2025",
-    },
-]
 
+@app.get("/users") 
+def get_user(name: str = None ):
+    return {"Name":name}
 
-@app.get("/", response_class=HTMLResponse, include_in_schema=False)
-@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
-def home():
-    return f"<h1>{posts[0]['title']}</h1>"
+@app.get("/products")
+def get_user(limit: int = 10):
+    return {"limit":limit}
 
-@app.get("/api/posts")
-def get_posts():
-    return posts
+@app.get("/items")
+def get_user(name:str=None,price: int=0):
+    return {
+        "Name":name,
+        "price":price
+    }
