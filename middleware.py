42e1e7d382d4764a8b9facf9e084839e05b813b6
Aditya-kr-007 <adikumar681@gmail.com>
2026-08-20 20:40:22 +0530

    add

---
+++ middleware.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, Request
+import time
+
+app=FastAPI()
+
+@app.middleware("http")
+async def middleware_my(request:Request,call_next):
+    start_time= time.time()
+    responce= await call_next(request)
+    process_time=time.time()-start_time
+    print(f"Path:{request.url.path} | Time:{process_time}")
+    return responce
